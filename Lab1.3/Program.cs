8a0b9f0fcb71c0206e694c9fa170c9e6f8de357c
Александр Сенченко <senchenko.sashuta@list.ru>
2026-03-03 09:09:17 +0800

    Задача решена, осталось убарть лапшу и реализовать декомпозицию

--- Lab1.3/Program.cs
+++ Lab1.3/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Lab1._3
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab1._3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите предложение: ");
+            var proposal = Console.ReadLine();
+
+            var letterCount = 0;
+            for (int i = 0; i < proposal.Length; i++)
+            {
+                var c = proposal[i];
+                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= 'а' && c <= 'я') || (c >= 'А' && c <= 'Я'))
+                {
+                    letterCount++;
+                }
+            }
+
+            var percent = letterCount * 100 / proposal.Length;
+            Console.WriteLine("Доля букв в данном предложении равно: " + percent + "%");
         }
     }
 }
